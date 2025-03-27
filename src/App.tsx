5eb16fd22d1d56ed5ef6f0fbcb215525ea961b8c
GCell-droid <niteshparihar104@gmail.com>
2025-03-27 23:44:54 +0530

    testing commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter ,Route,Routes} from 'react-router'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
+
+function App() {
+  return (
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path='/' element={<Body/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
